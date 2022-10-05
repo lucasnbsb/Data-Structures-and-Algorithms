e2ff778cae7b5dc5be70aec68f1288144bd07531
Lucas Neto Moreira <lucascpd@unb.br>
2022-10-05 15:40:10 -0300

    big test case, drop the tail pointer idea

--- medium/707. Design Linked List.py
+++ medium/707. Design Linked List.py
@@ -28,7 +28,7 @@ class MyLinkedList:
 
     def addAtTail(self, val: int) -> None:
         if ((self.tail is None) and (self.head is None)):
-            self.addAthead(val)
+            self.addAtHead(val)
         else:
             node = Node(val, None)
             self.tail.next = node
@@ -36,7 +36,7 @@ class MyLinkedList:
             self.size += 1
 
     def addAtIndex(self, index: int, val: int) -> None:
-        if index < 0 or index >= self.size:
+        if index < 0 or index > self.size:
             return -1
 
         if index == 0:
@@ -63,7 +63,10 @@ class MyLinkedList:
             cur = self.head
             for i in range(index-1):
                 cur = cur.next
-            cur.next = cur.next.next
+            if cur.next:
+                cur.next = cur.next.next
+            else:
+                self.tail = cur
             self.size -= 1
 
 
@@ -74,3 +77,7 @@ class MyLinkedList:
 # obj.addAtTail(val)
 # obj.addAtIndex(index,val)
 # obj.deleteAtIndex(index)
+
+
+["MyLinkedList","addAtHead","addAtTail","addAtTail","get","get","addAtTail","addAtIndex","addAtHead","addAtHead","addAtTail","addAtTail","addAtTail","addAtTail","get","addAtHead","addAtHead","addAtIndex","addAtIndex","addAtHead","addAtTail","deleteAtIndex","addAtHead","addAtHead","addAtIndex","addAtTail","get","addAtIndex","addAtTail","addAtHead","addAtHead","addAtIndex","addAtTail","addAtHead","addAtHead","get","deleteAtIndex","addAtTail","addAtTail","addAtHead","addAtTail","get","deleteAtIndex","addAtTail","addAtHead","addAtTail","deleteAtIndex","addAtTail","deleteAtIndex","addAtIndex","deleteAtIndex","addAtTail","addAtHead","addAtIndex","addAtHead","addAtHead","get","addAtHead","get","addAtHead","deleteAtIndex","get","addAtHead","addAtTail","get","addAtHead","get","addAtTail","get","addAtTail","addAtHead","addAtIndex","addAtIndex","addAtHead","addAtHead","deleteAtIndex","get","addAtHead","addAtIndex","addAtTail","get","addAtIndex","get","addAtIndex","get","addAtIndex","addAtIndex","addAtHead","addAtHead","addAtTail","addAtIndex","get","addAtHead","addAtTail","addAtTail","addAtHead","get","addAtTail","addAtHead","addAtTail","get","addAtIndex"]
+[[],[84],[2],[39],[3],[1],[42],[1,80],[14],[1],[53],[98],[19],[12],[2],[16],[33],[4,17],[6,8],[37],[43],[11],[80],[31],[13,23],[17],[4],[10,0],[21],[73],[22],[24,37],[14],[97],[8],[6],[17],[50],[28],[76],[79],[18],[30],[5],[9],[83],[3],[40],[26],[20,90],[30],[40],[56],[15,23],[51],[21],[26],[83],[30],[12],[8],[4],[20],[45],[10],[56],[18],[33],[2],[70],[57],[31,24],[16,92],[40],[23],[26],[1],[92],[3,78],[42],[18],[39,9],[13],[33,17],[51],[18,95],[18,33],[80],[21],[7],[17,46],[33],[60],[26],[4],[9],[45],[38],[95],[78],[54],[42,86]]
